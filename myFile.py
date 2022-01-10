745014ab9e2302deb5bafe583a58a589c7ea525a
gabby-diaz <95875413+gabby-diaz@users.noreply.github.com>
2022-01-09 18:05:50 -0800

    Counting DNA nucleotides and figuring out github
    This was part of my second (60 min) work session. As you can see I am still figuring this out because I didn't enter in my journal entry for my first 45 minutes. The code is having issues but this is how far I've gotten in the hour. Plus I used some of this work session to try and figure out github. 
    Here is my journal entry from session one:
    I was learning how to use Github and also used a lot of my time researching which project to do. I spent a lot of my time relearning bioinformatics and how to approach the first problem.

--- myFile.py
+++ myFile.py
@@ -1,1 +1,29 @@
 #testing
+#counting dna nucleotides
+
+import resources
+
+def dnaCount(dnaSeq):
+	'''
+	Input: DNA Sequence
+	Output: four intigers A, C, G, T
+	'''
+	A = 0
+	T = 0
+	G = 0
+	C = 0
+
+	for nucleotide in dnaSeq:
+		if nucleotide == 'A':
+			A += 1
+		elif nucleotide == 'T':
+			T +=1
+		elif nucleotide == 'G':
+			G +=1
+		elif nucleotide == 'C':
+			C +=1
+
+	return(A, T, G, C)
+
+if __name__=='__main__':
+	print(dnaCount('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'))
