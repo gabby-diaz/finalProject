642226009e60dc1069dcee6b5cabf566e8f4413e
gabby-diaz <95875413+gabby-diaz@users.noreply.github.com>
2022-01-17 20:40:01 -0800

    finishing Counting DNA nucleotides, DNA to RNA
    January 17 (6:30-8:30)
    first hour
    Writing this journal entry as I go. I'm trying to redo my dnaCount function because it is not working. I pasted in my second try and commented out my first try. The second try, I'm 30 minutes in and I made it so that the code returns my DNA sequence (lol not supposed to happen). (false alarm I fixed it but the output is still (209, 241, 210, 239) so) I'm going to scratch this out but still, show it in the comments. And I'm going to try and have an extremely different approach to see if it works better. (another false alarm I actually just used the wrong data) sorry, this is so messy. What I thought was wrong was actually right it was just different from the sample datasets because I used the DNA files. The second try was a success(I spelled my own function wrong oops)! During this first hour, I also finished the DNA to RNA problem. It was pretty easy and quick as I had the format of the first problem to help me. \
    Second Hour
    I Started the REVC but I didn't finish. I keep getting the "KeyError: '\n'" but I have no idea what it means; next work session I'll research it more to understand the error message and fix my code. As I am working on all of these problems it is getting easier and easier for me to write functions and loops. I pasted in the progress I got on REVC.

--- myFile.py
+++ myFile.py
@@ -1,19 +1,47 @@
 #testing
 #counting dna nucleotides
+#first try
+# def dnaCount(dnaSeq):
+#	'''
+#	Input: DNA Sequence
+#	Output: four intigers A, C, G, T
+#	'''
+#	A = 0
+#	T = 0
+#	G = 0
+#	C = 0
 
-import resources
+#	for nucleotide in dnaSeq:
+#		if nucleotide == 'A':
+#			A += 1
+#		elif nucleotide == 'T':
+#			T +=1
+#		elif nucleotide == 'G':
+#			G +=1
+#		elif nucleotide == 'C':
+#			C +=1
+
+#	return(A, T, G, C)
+
+#if __name__=='__main__':
+#	print(dnaCount('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'))
+
+#second try on making the dna count thing work
+#problem 1 Counting DNA nucleotides - DNA
 
 def dnaCount(dnaSeq):
 	'''
 	Input: DNA Sequence
 	Output: four intigers A, C, G, T
 	'''
+	#
 	A = 0
 	T = 0
 	G = 0
 	C = 0
 
 	for nucleotide in dnaSeq:
+		#counting the amount of dna nucleotides in the sequence
 		if nucleotide == 'A':
 			A += 1
 		elif nucleotide == 'T':
@@ -25,5 +53,97 @@ def dnaCount(dnaSeq):
 
 	return(A, T, G, C)
 
+def readfile(file_path):
+	'''
+	input: file 
+	output: string of data needed
+	''' 
+	#so that the function can read the file with the dna data
+	with open(file_path) as f:
+		file_data = f.read()
+	return file_data
+
 if __name__=='__main__':
-	print(dnaCount('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'))
+	file_path ='rosalind_dna.txt'
+	#reading the data
+	dnaSeq = readfile(file_path)
+
+#printing the result
+	print(dnaCount(dnaSeq))
+	
+#transcribing dna into rna - RNA
+#1-17-22
+
+def DtoR(dnaSeq):
+    '''
+    input: DNA Sequence
+    output: RNA Sequence (Nucleotide T becomes U)
+    '''
+    rnaSeq = ''
+#making it so that every T becoemes a U and everything else stays the same
+    for nucleotide in dnaSeq:
+        if nucleotide == 'T':
+            rnaSeq += 'U'
+        else:
+            rnaSeq += nucleotide
+
+    return rnaSeq
+
+def readfile(file_path):
+	'''
+	input: file 
+	output: string of data needed
+	''' 
+	#so that the function can read the file with the dna data
+	with open(file_path) as f:
+		file_data = f.read()
+	return file_data
+
+if __name__ == '__main__':
+    file_path ='rosalind_dna.txt'
+	#reading the data
+dnaSeq = readfile(file_path)
+
+#printing the result
+print(DtoR(dnaSeq))
+
+#Complementing a strand of DNA -REVC
+
+def Rcompliment(dnaSeq):
+    '''
+    input: DNA Sequence
+    output: The reverse complimentary nucleotides of the DNA Sequence
+    '''
+    # A compliments T, G compliments C, vise versa
+    #reverse the dna sequence
+    dnaReverse = dnaSeq[::-1]
+
+    #find the compliments
+    Cnucleotide = {'A':'T', 'G':'C','T':'A','C':'G'}
+
+    Rcompliment = ''
+    for nucleotide in dnaReverse:
+        Rcompliment += Cnucleotide[nucleotide]
+
+    return(Rcompliment)
+
+def readfile(file_path):
+	'''
+	input: file 
+	output: string of data needed
+	''' 
+	#so that the function can read the file with the dna data
+	with open(file_path) as f:
+		file_data = f.read()
+	return file_data
+
+if __name__=='__main__':
+    file_path ='rosalind_dna.txt'
+	#reading the data
+dnaSeq = readfile(file_path)
+
+#printing the result
+print(Rcompliment(dnaSeq))
+
+	
+	
